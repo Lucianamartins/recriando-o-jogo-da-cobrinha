6a93c9cabc3e7cd64d79b4ec960bdd193aaf7fb7
Felipe Lira <felipe.silva@invent.eng.br>
2021-11-05 10:03:37 -0300

    refatoração preliminar na criação da comidinha

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 /*
  Bugs encontrados:
- 1) a cobra não aparece ao atravessar a parede inferior;
+ 1) a cobra não aparece ao atravessar a parede inferior; OK
  
- 2) as comidinhas conseguem aparecer no corpo da cobra;
+ 2) as comidinhas conseguem aparecer no corpo da cobra; OK
  
  3) a cobra consegue andar numa pequena "margem invisível"
     nas bordas do quadro verde antes de aparecer na parede
     contrária;
 */
 
-let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");
-let box = 32;
-let snake = [];
+const canvas = document.getElementById("snake");
+const context = canvas.getContext("2d");
+const box = 32;
 
-snake [0] =  {
-    x: 8 * box,
-    y: 8 * box
-}
-
-let direction = "rigth";
+let jogo;
+let snake;
+let direction = "right";
 
 let food = {
-    x: Math.floor(Math. random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box
+    x: 0,
+    y: 0
 }
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0,  16 * box, 16 * box);
 }
+
 /* aqui eu escondo a cobra */
 function criarCobrinha() {
 
@@ -40,44 +37,48 @@ function criarCobrinha() {
 
 }
 
- function drawfood(){
+ function drawfood(change) {
 
-     context.fillStyle = "red";
-     context.fillRect(food.x, food.y, box, box );
+    if(change) {
+        food.x = parseInt(Math.random() * 15 + 1) * box;
+        food.y = parseInt(Math.random() * 15 + 1) * box;
+    }
+
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box );
 
  }
 
 document.addEventListener('keydown' , update );
 
 function update (event) {
-    if(event.keyCode == 37 && direction != "rigth") direction = "left";
+    if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "rigth";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function resetarJogo() {
 
     snake = [];
+
     snake.push({
         x: 8 * box,
         y: 8 * box
-    })
+    });
 
-    direction = "rigth";
-    food = {
-        x: food.x = Math.floor(Math.random() * 15 + 1) * box,
-        y: food.y = Math.floor(Math.random() * 15 + 1) * box
-    }
-
+    direction = "right";
+    drawfood(true);
     jogo = null;
+
+    criarBG();
 
 }
 
 /* aqui é os movimentos, da cobra faz ela andar entre as paredes  */
-function iniciarJogo(){
+function iniciarJogo() {
 
-     if(snake[0].x > 15 * box && direction == "rigth" ) snake[0].x = 0;
+     if(snake[0].x > 15 * box && direction == "right" ) snake[0].x = 0;
      if(snake[0].x < 0 && direction == "left" ) snake[0].x = 16 * box;
      if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
      if(snake[0].y <  0 && direction == "up") snake[0].y = 16 * box;
@@ -89,26 +90,24 @@ function iniciarJogo(){
              resetarJogo();
          }
      }
+
     /* aqui eu chamo as funcoes*/
     criarBG(); 
     criarCobrinha();
     drawfood();
 
     let snakeX = snake[0].x;
-    let snakeY= snake[0].y;
+    let snakeY = snake[0].y;
 
-    if(direction == "rigth" ) snakeX += box;
+    if(direction == "right" ) snakeX += box;
     if(direction == "left" ) snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-
     if(snakeX != food.x || snakeY != food.y)
         snake.pop();
-    else {
-        food.x = Math.floor(Math.random() * 15 + 1) * box;
-        food.y = Math.floor(Math.random() * 15 + 1) * box;
-    }
+    else
+        drawfood(true);
 
     let newHead = {
         x: snakeX,
@@ -119,12 +118,9 @@ function iniciarJogo(){
 
 }
 
-
-let jogo;
-criarBG();
+resetarJogo();
 
 document.addEventListener('keydown', () => {
     if(!jogo)
         jogo = setInterval(iniciarJogo, 75);    
 });
- 
